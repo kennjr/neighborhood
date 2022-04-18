6810ba0a75ba2826035e6a86e90b6b3a285d81ce
kennjr <ken.juniour@student.moringaschool.com>
2022-04-18 09:28:24 +0300

    Added the members table to the admin dashboard

--- hood/admin.py
+++ hood/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 
 # Register your models here.
-from hood.models import Profile
+from hood.models import Profile, Neighborhood, Member
 
 
 class ProfileAdmin(admin.ModelAdmin):
@@ -16,4 +16,28 @@ class ProfileAdmin(admin.ModelAdmin):
     # prepopulated_fields = {'slug': ('title',)}
 
 
+class NeighborhoodAdmin(admin.ModelAdmin):
+    # The line below will allow us to display the meetup obj items in a list with the specified columns
+    # the columnnames in the models are what we've used in the strs
+    list_display = ('name', 'location', 'members_count')
+    # The line below will allow us to have filter opts for our list of entries that appear in the admin dash
+    list_filter = ('members_count', )
+    # The line below will pre-populate the slug field based on the title we enter,
+    #  the key is the param that we'd like to pre-populate and the tuple has the entries that'll be concat'ed to create the key
+    # prepopulated_fields = {'slug': ('title',)}
+
+
+class MemberAdmin(admin.ModelAdmin):
+    # The line below will allow us to display the meetup obj items in a list with the specified columns
+    # the columnnames in the models are what we've used in the strs
+    # list_display = ('name', 'location', 'members_count')
+    # The line below will allow us to have filter opts for our list of entries that appear in the admin dash
+    list_filter = ('status', )
+    # The line below will pre-populate the slug field based on the title we enter,
+    #  the key is the param that we'd like to pre-populate and the tuple has the entries that'll be concat'ed to create the key
+    # prepopulated_fields = {'slug': ('title',)}
+
+
 admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Neighborhood, NeighborhoodAdmin)
+admin.site.register(Member, MemberAdmin)
