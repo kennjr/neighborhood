e550b88c23116a6377402cd4c9c455cae2f504a6
kennjr <ken.juniour@student.moringaschool.com>
2022-04-19 01:18:35 +0300

    Registered the post model class to the admin dashboard

--- hood/admin.py
+++ hood/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 
 # Register your models here.
-from hood.models import Profile, Neighborhood, Member
+from hood.models import Profile, Neighborhood, Member, Post
 
 
 class ProfileAdmin(admin.ModelAdmin):
@@ -38,6 +38,18 @@ class MemberAdmin(admin.ModelAdmin):
     # prepopulated_fields = {'slug': ('title',)}
 
 
+class PostAdmin(admin.ModelAdmin):
+    # The line below will allow us to display the meetup obj items in a list with the specified columns
+    # the columnnames in the models are what we've used in the strs
+    list_display = ('message', 'image')
+    # The line below will allow us to have filter opts for our list of entries that appear in the admin dash
+    list_filter = ('user', )
+    # The line below will pre-populate the slug field based on the title we enter,
+    #  the key is the param that we'd like to pre-populate and the tuple has the entries that'll be concat'ed to create the key
+    # prepopulated_fields = {'slug': ('title',)}
+
+
 admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Neighborhood, NeighborhoodAdmin)
 admin.site.register(Member, MemberAdmin)
+admin.site.register(Post, PostAdmin)
