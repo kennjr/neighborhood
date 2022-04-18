ab3c30549974c1c74827bff25ef75d3c9fac0f41
kennjr <ken.juniour@student.moringaschool.com>
2022-04-18 13:11:07 +0300

    Created a new url for the join btn fun i.e selected_neighborhood_page

--- hood/urls.py
+++ hood/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login/', views.login_page, name="login_page"),
     path('register/', views.register_page, name="register_page"),
     path('create-profile/', views.create_profile_page, name="create_profile_page"),
-    path('neighborhoods/', views.neighborhoods_page, name="neighborhoods_page")
+    path('neighborhoods/', views.neighborhoods_page, name="neighborhoods_page"),
+    path('neighborhoods/<int:n_id>', views.selected_neighborhood_page, name="selected_neighborhood_page"),
 ]
