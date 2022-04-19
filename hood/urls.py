924086f08fef17654a8df0dfe4953b2398db69b7
kennjr <ken.juniour@student.moringaschool.com>
2022-04-19 10:25:59 +0300

    Added the leave url to the urlpatterns list

--- hood/urls.py
+++ hood/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('register/', views.register_page, name="register_page"),
     path('create-profile/', views.create_profile_page, name="create_profile_page"),
     path('neighborhoods/', views.neighborhoods_page, name="neighborhoods_page"),
-    path('neighborhoods/<int:n_id>', views.selected_neighborhood_page, name="selected_neighborhood_page"),
+    path('neighborhoods/<int:n_id>/', views.selected_neighborhood_page, name="selected_neighborhood_page"),
+    path('search/', views.search_page, name='search_page'),
+    path('leave/', views.leave_community_page, name="leave_community_page"),
 ]
