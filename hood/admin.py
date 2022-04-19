b965e052acf74b8c8e728dff01d6b145bcff7432
kennjr <ken.juniour@student.moringaschool.com>
2022-04-19 10:24:35 +0300

    Added the business class to the admin dashboard

--- hood/admin.py
+++ hood/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 
 # Register your models here.
-from hood.models import Profile, Neighborhood, Member, Post
+from hood.models import Profile, Neighborhood, Member, Post, Business
 
 
 class ProfileAdmin(admin.ModelAdmin):
@@ -49,7 +49,19 @@ class PostAdmin(admin.ModelAdmin):
     # prepopulated_fields = {'slug': ('title',)}
 
 
+class BusinessAdmin(admin.ModelAdmin):
+    # The line below will allow us to display the meetup obj items in a list with the specified columns
+    # the columnnames in the models are what we've used in the strs
+    list_display = ('name', 'phone')
+    # The line below will allow us to have filter opts for our list of entries that appear in the admin dash
+    list_filter = ('location', )
+    # The line below will pre-populate the slug field based on the title we enter,
+    #  the key is the param that we'd like to pre-populate and the tuple has the entries that'll be concat'ed to create the key
+    # prepopulated_fields = {'slug': ('title',)}
+
+
 admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Neighborhood, NeighborhoodAdmin)
 admin.site.register(Member, MemberAdmin)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Business, BusinessAdmin)
